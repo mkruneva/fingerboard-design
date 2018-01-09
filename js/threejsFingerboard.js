aa1f2093ceb5a51dbf5b20d427894f203182c23a
mkruneva <marta.kruneva@gmail.com>
2018-01-09 19:51:18 +0000

    5 trasparent boxes, next step - remember position and change colour on click

--- js/threejsFingerboard.js
+++ js/threejsFingerboard.js
@@ -4,10 +4,6 @@ let camera;
 let controls;
 let scene;
 let renderer;
-let sprite;
-let spriteBehindObject;
-const annotation = document.querySelector(".annotation");
-const annotation2 = document.querySelector(".sloper20");
 
 init();
 animate();
@@ -19,139 +15,49 @@ function init() {
     camera.position.set(0, 0, 750);
 
     //RENDER
-    renderer = createRenderer(0x222222);
+    renderer = createRenderer(0xDDDDDD);
     var parent = document.getElementById('canvasContainer');
     parent.appendChild(renderer.domElement);
 
-    //ANNOTATIONS
-    // const canvas = document.createElement('canvas');
-    // const context = canvas.getContext('2d');
-    // console.log(context);
-
-    // 30* sloper annotation 
-    // var spriteMap = new THREE.TextureLoader().load("tex/annotations/1.png");
-    // var spriteMaterial = new THREE.SpriteMaterial({ map: spriteMap, color: 0xffffff });
-    // var sprite = new THREE.Sprite(spriteMaterial);
-    // sprite.position.set(-176, 66, 50);
-    // sprite.scale.set(20, 20, 1);
-    // scene.add(sprite);
-    //console.log("sprite is ", sprite);
-
-    createSprite(-176, 66, 50); //sloper 30 degrees
-    createSprite(-87, 66, 50); //sloper 20 degrees
-
-    // //SPTITE GUI
-    // var gui = new dat.GUI();
-    // var spriteGui = gui.addFolder('Sprite position');
-    // spriteGui.add(sprite_20deg.position, 'x', -500, 500);
-    // spriteGui.add(sprite_20deg.position, 'y', -500, 500);
-    // spriteGui.add(sprite_20deg.position, 'z', -500, 500);
-
     //MATERIALS
-    var backgroundMat = createBackgroundMaterial();
-    var fingerboardMat = createFingerBoardMaterial();
+    //var backgroundMat = createBackgroundMaterial();
+    //var fingerboardMat = createFingerBoardMaterial();
 
     //LIGHTS
     createLights();
 
     //CREATE BACKGROUND PLANE 
-    createBackgroundPlane(backgroundMat);
+    //createBackgroundPlane(backgroundMat);
 
     //INVISIBLE CUBE
-    var invisibleCube = createInvisibleBox(622, 154, 64);
+    //var invisibleCube = createInvisibleBox(622, 154, 64);
+    //var cubeAll = createInvisibleBox(540, 150, 60, 0xFFFFFF, 0.1);
+    var cube1 = createInvisibleBox(90, 150, 60, 0xFF00FF, 0.5); cube1.position.setX(-180);
+    var cube2 = createInvisibleBox(90, 150, 60, 0x00FFFF, 0.5); cube2.position.setX(-90);
+    var cube3 = createInvisibleBox(90, 150, 60, 0xFFFF00, 0.5); cube3.position.setX(0)
+    var cube4 = createInvisibleBox(90, 150, 60, 0x0000FF, 0.5); cube4.position.setX(90);
+    var cube5 = createInvisibleBox(90, 150, 60, 0x00FF00, 0.5); cube5.position.setX(180);
 
     //LOAD FINGERBOARD
-    loadObject('obj/fingerboard-obj.obj', fingerboardMat);
+    //loadObject('obj/fingerboard-obj.obj', fingerboardMat);
 
     //Controls
     controls = new THREE.OrbitControls(camera, renderer.domElement);
 
     //LOADER
-    loadingScreen();
+    //loadingScreen();
 }
 
 //FUNCTIONS
 
-//Annotations and Sprites 
-function createSprite(x, y, z, scale) {
-    var spriteMap = new THREE.TextureLoader().load("tex/annotations/1.png");
-    var spriteMaterial = new THREE.SpriteMaterial({ map: spriteMap, color: 0xffffff });
-    var sprite = new THREE.Sprite(spriteMaterial);
-    sprite.position.set(x, y, z);
-    sprite.scale.set(20, 20, 1);
-    scene.add(sprite);
-
-    return sprite;
-}
 //Materials and Textures
-function repeatTex(mapName, repeat) {
-    mapName.wrapS = THREE.RepeatWrapping;
-    mapName.wrapT = THREE.RepeatWrapping;
-    mapName.repeat.set(repeat, repeat);
-}
 
-function loadTextures() {
-    var maps = {
-        diffTex: new THREE.TextureLoader().load("tex/beech_wood_albedo.jpg"),
-        aoTex: new THREE.TextureLoader().load("tex/beech_wood_ao.png"),
-        nrmTex: new THREE.TextureLoader().load("tex/beech_wood_anormal.png"),
-        roughtTex: new THREE.TextureLoader().load("tex/beech_wood_rough.png"),
-        backgroundTex: new THREE.TextureLoader().load("tex/background.jpg"),
-    }
-    repeatTex(maps.diffTex, 3);
-    repeatTex(maps.nrmTex, 3);
-    repeatTex(maps.backgroundTex, 3);
-    maps.envCubeMap = new THREE.CubeTextureLoader()
-        .setPath('tex/cubemap/')
-        .load([
-            'posx.jpg',
-            'negx.jpg',
-            'posy.jpg',
-            'negy.jpg',
-            'posz.jpg',
-            'negz.jpg',
-        ]);
-    return maps;
-}
-
-function createFingerBoardMaterial() {
-    var textureContainer = loadTextures();
-    var material = new THREE.MeshStandardMaterial({
-        aoMap: textureContainer.aoTex,
-        color: 0xbbbbbb,
-        envMap: textureContainer.envCubeMap,
-        map: textureContainer.diffTex,
-        normalMap: textureContainer.nrmTex,
-        normalScale: new THREE.Vector3(0.3, 0.3),
-        roughness: 0.98,
-        roughnessMap: textureContainer.roughtTex
-    });
-
-    return material;
-}
-
-function createBackgroundMaterial() {
-    var textureContainer = loadTextures();
-    var material = new THREE.MeshBasicMaterial({ color: 0xAAAAAA, map: textureContainer.backgroundTex });
-
-    return material;
-}
-
-function createBackgroundPlane(material) {
-    var geometry = new THREE.PlaneGeometry(3000, 3000);
-    var plane = new THREE.Mesh(geometry, material);
-    plane.position.set(0, 0, -1400);
-    camera.add(plane);
-
-    return plane;
-}
-
-function createInvisibleBox(width, height, depth) {
+function createInvisibleBox(width, height, depth, colorVal, opacityVal) {
     var geometry = new THREE.CubeGeometry(width, height, depth);
-    var material = new THREE.MeshBasicMaterial({ color: 0x0000FF });
+    var material = new THREE.MeshLambertMaterial({ color: colorVal, transparent: true, opacity: opacityVal });
     var cube = new THREE.Mesh(geometry, material);
-    cube.position.set(0, -14, 31);
-    cube.visible = false; //hiding the Cube
+    cube.position.set(0, 80, 31);
+    //cube.visible = false; //hiding the Cube
     scene.add(cube);
 
     return cube;
@@ -233,49 +139,6 @@ function createRenderer(clearColour) {
     return myRenderer;
 }
 
-function updateAnnotationOpacity() {
-
-    //WORKAROUND - invisibleCube and Sprite - not defined 
-    //console.log(scene.children);
-    var sprite = scene.children[1];
-    var invCube = scene.children[6];
-    const meshDistance = camera.position.distanceTo(invCube.position);
-    const spriteDistance = camera.position.distanceTo(sprite.position);
-    spriteBehindObject = spriteDistance > meshDistance;
-    sprite.material.opacity = spriteBehindObject ? 0.25 : 1;
-
-    // const meshDistance = camera.position.distanceTo(invisibleCube.position);
-    // const spriteDistance = camera.position.distanceTo(sprite.position);
-    // spriteBehindObject = spriteDistance > meshDistance;
-    // sprite.material.opacity = spriteBehindObject ? 0.25 : 1;
-
-    // Do you want a number that changes size according to its position?
-    // Comment out the following line and the `::before` pseudo-element.
-    // sprite.material.opacity = 0;
-}
-
-function updateScreenPosition() {
-    const vector = new THREE.Vector3(-176, 66, 50); // sprite position
-    const canvas = renderer.domElement;
-
-    vector.project(camera);
-    vector.x = Math.round((0.5 + vector.x / 2) * (canvas.clientWidth / window.devicePixelRatio));
-    vector.y = Math.round((0.5 - vector.y / 2) * (canvas.clientHeight / window.devicePixelRatio)); //changed from canvas.height to canvas.clienntHeight
-
-    annotation.style.top = `${vector.y}px`;
-    annotation.style.left = `${vector.x}px`;
-    annotation.style.opacity = spriteBehindObject ? 0.25 : 1;
-
-    const vector2 = new THREE.Vector3(-87, 66, 50); // sprite position 
-    vector2.project(camera);
-    vector2.x = Math.round((0.5 + vector2.x / 2) * (canvas.clientWidth / window.devicePixelRatio));
-    vector2.y = Math.round((0.5 - vector2.y / 2) * (canvas.clientHeight / window.devicePixelRatio));
-
-    annotation2.style.top = `${vector2.y}px`;
-    annotation2.style.left = `${vector2.x}px`;
-    annotation2.style.opacity = spriteBehindObject ? 0.25 : 1;
-}
-
 function animate() {
     requestAnimationFrame(animate);
     controls.update();
@@ -284,6 +147,4 @@ function animate() {
 
 function render() {
     renderer.render(scene, camera);
-    updateAnnotationOpacity();
-    updateScreenPosition();
 }
